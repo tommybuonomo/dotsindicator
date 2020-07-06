532d2e2e279293c75fe3c828bd72556f93d5311c
John Lombardo <john@lombardos.org>
2020-07-06 14:40:11 -0700

    New refresh button to test upates to the underlying adapter

--- viewpagerdotsindicator-sample/src/main/java/com/tbuonomo/dotsindicatorsample/viewpager2/ViewPager2Activity.kt
+++ viewpagerdotsindicator-sample/src/main/java/com/tbuonomo/dotsindicatorsample/viewpager2/ViewPager2Activity.kt
@@ -1,6 +1,7 @@
 package com.tbuonomo.dotsindicatorsample.viewpager2
 
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,11 @@ import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
 
 class ViewPager2Activity : AppCompatActivity() {
 
+  val adapter by lazy { DotIndicatorPager2Adapter() }
+  val dotsIndicator by lazy { findViewById<DotsIndicator>(R.id.dots_indicator) }
+  val springDotsIndicator by lazy { findViewById<SpringDotsIndicator>(R.id.spring_dots_indicator) }
+  val wormDotsIndicator by lazy { findViewById<WormDotsIndicator>(R.id.worm_dots_indicator) }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -20,12 +26,7 @@ class ViewPager2Activity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
     setContentView(R.layout.activity_view_pager2)
 
-    val dotsIndicator = findViewById<DotsIndicator>(R.id.dots_indicator)
-    val springDotsIndicator = findViewById<SpringDotsIndicator>(R.id.spring_dots_indicator)
-    val wormDotsIndicator = findViewById<WormDotsIndicator>(R.id.worm_dots_indicator)
-
     val viewPager2 = findViewById<ViewPager2>(R.id.view_pager2)
-    val adapter = DotIndicatorPager2Adapter()
     viewPager2.adapter = adapter
 
     val zoomOutPageTransformer = ZoomOutPageTransformer()
@@ -37,4 +38,13 @@ class ViewPager2Activity : AppCompatActivity() {
     springDotsIndicator.setViewPager2(viewPager2)
     wormDotsIndicator.setViewPager2(viewPager2)
   }
+
+
+  fun refresh(view: View) {
+    adapter.refresh()
+    dotsIndicator.refreshDots()
+    springDotsIndicator.refreshDots()
+    wormDotsIndicator.refreshDots()
+  }
+
 }
