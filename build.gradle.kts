9e58fdcef44fee402587cb2e847b05ee4536e435
Tommy Buonomo <tommy.buonomo@gmail.com>
2022-04-30 20:08:57 +0200

    Add mavenCentral repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 buildscript {
     repositories {
         google()
+        mavenCentral()
         jcenter()
     }
     dependencies {
@@ -12,6 +13,7 @@ buildscript {
 allprojects {
     repositories {
         google()
+        mavenCentral()
         jcenter()
     }
 }
