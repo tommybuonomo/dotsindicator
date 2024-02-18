de094be4af3444e06be3dcbd9d5f9ca08f46d256
ahmadizaz <ahmadizaz1996@gmail.com>
2024-02-19 00:28:16 +0100

    Upgraded the AGP to 8.2.2 and Gradle to 8.2 for support on Android Studio Hedgehog | 2023.1.1 Patch 2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 const val kotlinVersion = "1.8.10"
 
 object BuildPlugins {
-    val gradlePlugin = "com.android.tools.build:gradle:8.1.0-beta02"
+    val gradlePlugin = "com.android.tools.build:gradle:8.2.2"
     val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
 }
